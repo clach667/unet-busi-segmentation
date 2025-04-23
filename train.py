417929145d5bde36c4aae73e32857d6f863ecabe
clach667 <clara.choukroun@eleve.isep.fr>
2025-04-23 17:07:24 +0200

    add of tqdm for epoch

--- train.py
+++ train.py
@@ -2,9 +2,10 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from torch.utils.data import DataLoader
+from tqdm import tqdm
 
 from src.dataset import BUSIDataset
-from src.unet import UNet
+from src.unet import MiniUNet
 
 from torchsummary import summary
 
@@ -20,7 +21,7 @@ train_dataset = BUSIDataset("data/images", "data/masks", size=IMAGE_SIZE)
 train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
 
 # Initialize U-Net model
-model = UNet().to(DEVICE)
+model = MiniUNet().to(DEVICE)
 
 # Print model summary
 summary(model, input_size=(1, IMAGE_SIZE[0], IMAGE_SIZE[1]))
@@ -34,8 +35,9 @@ for epoch in range(EPOCHS):
     print("Training started")
     model.train()
     epoch_loss = 0.0
+    loop = tqdm(train_loader, desc=f"Epoch [{epoch+1}/{EPOCHS}]", leave=True)
 
-    for images, masks in train_loader:
+    for images, masks in loop:
         images = images.to(DEVICE)
         masks = masks.to(DEVICE)
 
@@ -47,5 +49,6 @@ for epoch in range(EPOCHS):
         optimizer.step()
 
         epoch_loss += loss.item()
+        loop.set_postfix(loss=loss.item())
 
     print(f"Epoch [{epoch+1}/{EPOCHS}] - Loss: {epoch_loss / len(train_loader):.4f}")
