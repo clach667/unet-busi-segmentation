edc1aacc6be536bad9728412189055aec2c7d4b1
clach667 <clara.choukroun@eleve.isep.fr>
2025-04-23 17:03:35 +0200

    add of MiniUnet : a simpler model for computer without GPU

--- src/unet.py
+++ src/unet.py
@@ -59,8 +59,49 @@ class UNet(nn.Module):
 
         return torch.sigmoid(self.final(dec1))
 
+class MiniUNet(nn.Module):
+    def __init__(self):
+        super(MiniUNet, self).__init__()
+
+        def conv_block(in_c, out_c):
+            return nn.Sequential(
+                nn.Conv2d(in_c, out_c, kernel_size=3, padding=1),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(out_c, out_c, kernel_size=3, padding=1),
+                nn.ReLU(inplace=True)
+            )
+
+        self.enc1 = conv_block(1, 32)
+        self.pool1 = nn.MaxPool2d(2)
+
+        self.enc2 = conv_block(32, 64)
+        self.pool2 = nn.MaxPool2d(2)
+
+        self.bottleneck = conv_block(64, 128)
+
+        self.upconv2 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.dec2 = conv_block(128, 64)
+
+        self.upconv1 = nn.ConvTranspose2d(64, 32, kernel_size=2, stride=2)
+        self.dec1 = conv_block(64, 32)
+
+        self.final = nn.Conv2d(32, 1, kernel_size=1)
+
+    def forward(self, x):
+        enc1 = self.enc1(x)
+        enc2 = self.enc2(self.pool1(enc1))
+        bottleneck = self.bottleneck(self.pool2(enc2))
+
+        dec2 = self.upconv2(bottleneck)
+        dec2 = self.dec2(torch.cat([dec2, enc2], dim=1))
+
+        dec1 = self.upconv1(dec2)
+        dec1 = self.dec1(torch.cat([dec1, enc1], dim=1))
+
+        return torch.sigmoid(self.final(dec1))
+    
 if __name__ == "__main__":
-    model = UNet()
+    model = MiniUNet()
     x = torch.randn((1, 1, 256, 256))  # 1 image grayscale 256x256
     y = model(x)
     print(y.shape)  # → torch.Size([1, 1, 256, 256])
